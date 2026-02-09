d8f17d98d2ad24da75ded35e9022f5a4fc17bdcb
Sumitkumarmandal <sumitkumarmandal1020@gmail.com>
2026-02-09 17:50:59 +0530

    creating the user model

--- src/main/java/com/example/ecobazaar/model/User.java
+++ src/main/java/com/example/ecobazaar/model/User.java
@@ -23,4 +23,51 @@ public class User {
     private String role ;
     private Integer ecoScore ;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Integer getEcoScore() {
+        return ecoScore;
+    }
+
+    public void setEcoScore(Integer ecoScore) {
+        this.ecoScore = ecoScore;
+    }
 }
