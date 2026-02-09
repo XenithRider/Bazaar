c3e9e6db97be7668867a6c2b796f297c139be9bc
Sumitkumarmandal <sumitkumarmandal1020@gmail.com>
2026-02-09 17:31:02 +0530

    Initial commit

---
+++ src/main/java/com/example/ecobazaar/EcobazaarApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecobazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcobazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcobazaarApplication.class, args);
+	}
+
+}
